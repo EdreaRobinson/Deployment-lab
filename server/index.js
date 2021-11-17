23087d20c3e89e1bc0e4a82e1aa65b129010801f
Edrea Robinson <edrea1979@gmail.com>
2021-11-17 15:26:11 -0600

    Styling

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../index.html'));
 })
 
+app.get("/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "../styles.css"));
+  });
+
 
 
 
